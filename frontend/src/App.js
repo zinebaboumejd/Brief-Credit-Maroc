75f7bb6735483d19a7340fff76376aef139565f3
zinebaboumejd <zinebaboumejd@gmail.com>
2023-01-16 15:06:01 +0100

    protect route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,9 @@ import GetClinet from "./pages/Admin/GetClinet"
 import Register from "./pages/Auth/Register";
 import ProfileClient from "./pages/Client/ProfileClient"
 import Releve from "./pages/Client/Releve";
+import Page404 from "./pages/Page404";
 import { ToastContainer } from 'react-toastify';
+
 
 function App() {
   const token=localStorage.getItem('token')
@@ -26,17 +28,39 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-      {/* nested route admin  */}
-          <Route path="/admin" element={<Dashboard />}>
-            
-            <Route path="/admin/getclient" element={<GetClinet />} />
-          </Route> 
-
+ <Route path="/register" element={<Register />} />
+      {/* protecte route si tocken egal admin  exest dashboard et tocken =client  profil sinon redaircete veir page 404 */}
+          {/* <Route path="/dashboard" element={token ? <Dashboard /> : <Page404 />} />
+          <Route path="/getclient" element={token ? <GetClinet /> : <Page404 />} />
+          <Route path="/profileclient" element={token ? <ProfileClient /> : <Page404 />} />
+          <Route path="/releve" element={token ? <Releve /> : <Page404 />} />
+          <Route path="*" element={<Page404 />} /> */}
+          {
+            token && token==='admin' ? 
+            <>
+            <Route path="/dashboard" element={<Dashboard />} /> 
+            <Route path="/getclient" element={<GetClinet />} />
+            </>
+            : 
+            <>
+            <Route path="/dashboard" element={<Login />} />
+            <Route path="/getclient" element={<Login />} />
+            </>
+         }
+          {
+            token && token==='client' ?
+            <>
+            <Route path="/profileclient" element={<ProfileClient />} />
+            <Route path="/releve" element={<Releve />} />
+            </>
+            :
+            <>
+            <Route path="/profileclient" element={<Login />} />
+            <Route path="/releve" element={<Login />} />
+            </>
+          }
+          <Route path="*" element={<Page404 />} />
           
-          <Route path="/register" element={<Register />} />
-          
-          <Route path="/profileclient" element={<ProfileClient />} />
-          <Route path="/releve" element={<Releve />} />
         </Routes>
       </Router>
 
